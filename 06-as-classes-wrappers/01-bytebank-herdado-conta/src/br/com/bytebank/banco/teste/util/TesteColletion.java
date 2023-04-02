36f72351eb42e4b81deca9b19d6ed8a03f0a6ab8
Gabriel Marques <bielmarques922@gmail.com>
2023-04-02 10:49:07 -0300

    Java - Java.Util - Modulo 06 - As classes Wrappers
    Modulo 06 - Autoboxing e Unboxing

---
+++ 06-as-classes-wrappers/01-bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/TesteColletion.java
@@ -0,0 +1,41 @@
+package br.com.bytebank.banco.teste.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
+	public class TesteColletion {
+	
+		public static void main(String[] args) {
+	
+	    //Collection<Conta> lista = new Vector<Conta>();
+	    List<Conta> lista = new Vector<Conta>();
+	    
+	    Conta cc = new ContaCorrente(22, 11);
+	    lista.add(cc);
+	
+	    Conta cc2 = new ContaCorrente(22, 22);
+	    lista.add(cc2);
+	    
+	    Conta cc3 = new ContaCorrente(22, 33);
+	    lista.add(cc3);
+	    
+	    System.out.println("Tamanho: "  + lista.size());
+        
+        for(int i = 0; i < lista.size(); i++) {
+        	Object oRef = lista.get(i);
+        	System.out.println(oRef);
+        }
+        
+        System.out.println("---------------------------");
+        
+        for(Object oRef : lista) {
+        	System.out.println(oRef);
+        }
+	}
+}
